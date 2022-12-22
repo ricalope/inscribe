3dffc4ac68d8a886642c85eaf66e7256b6081c8e
ricalope <mrrobot421@gmail.com>
2022-12-21 19:18:19 -0700

    setup modal-context

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
@@ -15,57 +15,57 @@ import DeleteNote from './components/DeleteNote';
 import { authenticate } from './store/session';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
+    const [ loaded, setLoaded ] = useState(false);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async() => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
+    useEffect(() => {
+        (async () => {
+            await dispatch(authenticate());
+            setLoaded(true);
+        })();
+    }, [ dispatch ]);
 
-  if (!loaded) {
-    return null;
-  }
+    if (!loaded) {
+        return null;
+    }
 
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path='/login'>
-          <LoginForm />
-        </Route>
-        <Route exact path='/sign-up'>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute exact path='/users/:userId'>
-          <User />
-        </ProtectedRoute>
-        <Route exact path='/'>
-          <h1>My Home Page</h1>
-        </Route>
-        <ProtectedRoute exact path='/notes'>
-            <Notes />
-        </ProtectedRoute>
-        <ProtectedRoute exact path="/notes/new">
-            <AddNote />
-        </ProtectedRoute>
-        <ProtectedRoute exact path="/notes/:noteId">
-            <OneNote />
-        </ProtectedRoute>
-        <ProtectedRoute exact path="/notes/:noteId/edit">
-            <EditNote />
-        </ProtectedRoute>
-        <ProtectedRoute exact path="/notes/:noteId/delete">
-            <DeleteNote />
-        </ProtectedRoute>
-        <Route>
-            <h2>Page Not Found</h2>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route exact path='/login'>
+                    <LoginForm />
+                </Route>
+                <Route exact path='/sign-up'>
+                    <SignUpForm />
+                </Route>
+                <ProtectedRoute exact path='/users/:userId'>
+                    <User />
+                </ProtectedRoute>
+                <Route exact path='/'>
+                    <h1>My Home Page</h1>
+                </Route>
+                <ProtectedRoute exact path='/notes'>
+                    <Notes />
+                </ProtectedRoute>
+                <ProtectedRoute exact path="/notes/new">
+                    <AddNote />
+                </ProtectedRoute>
+                <ProtectedRoute exact path="/notes/:noteId">
+                    <OneNote />
+                </ProtectedRoute>
+                <ProtectedRoute exact path="/notes/:noteId/edit">
+                    <EditNote />
+                </ProtectedRoute>
+                <ProtectedRoute exact path="/notes/:noteId/delete">
+                    <DeleteNote />
+                </ProtectedRoute>
+                <Route>
+                    <h2>Page Not Found</h2>
+                </Route>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
