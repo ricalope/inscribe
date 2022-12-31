8e0fa4ff2f126589971b36cb70e8cc2affe18f51
ricalope <mrrobot421@gmail.com>
2022-12-30 21:35:03 -0700

    added styling to notebooks page

--- react-app/src/components/Notebooks/index.js
+++ react-app/src/components/Notebooks/index.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getAllNotebooksThunk } from '../../store/notebook';
+import NavBar from '../Navigation/NavBar';
+import './Notebooks.css';
 
 
 function Notebooks() {
     const dispatch = useDispatch();
 
-    const notebooksObj = useSelector(state => state.notebooks.allNotebooks)
+    const notebooksObj = useSelector(state => state.notebooks.allNotebooks);
     const notebooks = Object.values(notebooksObj)
 
     useEffect(() => {
@@ -17,18 +19,49 @@ function Notebooks() {
     }, [ dispatch ])
 
     return (
-        <div className="notebooks-main-container">
-            <h2>Notebooks</h2>
-            {notebooks.map(notebook => (
-                <div key={notebook.id} className="notebooks-card">
-                    <div className="notebooks-title">
-                        <Link exact="true" to={`/notebooks/${notebook.id}`}>
-                            {notebook.title}
-                        </Link>
+        <>
+            <NavBar />
+            <div className="notebooks-main-container">
+                <div className="notebooks-inner">
+                    <h2>Notebooks</h2>
+                    <div className="length-new-div">
+                        <div className="length-nb">
+                            {notebooks.length} {notebooks.length === 1 ? 'notebook' : 'notebooks'}
+                        </div>
+                        <div className="new-nb">
+                            <Link exact="true" to="/notebooks/new" id="new-nb-link">
+                                <div><i className="fa-solid fa-folder-plus" /></div>
+                                <div>New Notebook</div>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="notebooks-table">
+                        <div className="notebooks-header-column">
+                            <div id="t-header">Title</div>
+                            {notebooks.map(notebook => (
+                                <div key={notebook.id} className="notebooks-header">
+                                    <Link
+                                        exact="true"
+                                        to={`/notebooks/${notebook.id}`}
+                                        id="nb-one-link">
+                                        <div><i className="fa-solid fa-book" /></div>
+                                        <div>{`${notebook.title} (${notebook.notes.length})`}</div>
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="notebooks-body-rows">
+                            <div id="t-created">Created By</div>
+                            {notebooks.map(notebook => (
+                                <div key={notebook.id} className="notebooks-rows">
+                                    {notebook.user_email}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
-            ))}
-        </div>
+            </div>
+        </>
     )
 }
 
