11bbf88b44e96db8156720573e978081c3174479
ricalope <mrrobot421@gmail.com>
2023-01-02 15:56:33 -0700

    added styling for add-notebook modals

--- react-app/src/components/AddNotebook/index.js
+++ react-app/src/components/AddNotebook/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addNotebookThunk } from '../../store/notebook';
+import './AddNotebook.css';
 
 
 function AddNotebook({ setShowNew }) {
     const dispatch = useDispatch();
 
-    const [title, setTitle] = useState('');
+    const [ title, setTitle ] = useState('');
 
     const onSubmit = async (e) => {
         e.preventDefault()
@@ -22,24 +23,44 @@ function AddNotebook({ setShowNew }) {
             <form onSubmit={onSubmit}>
                 <div className="add-nb-form-container">
                     <div className="add-nb-header">
-                        <h3>Create new notebook</h3>
+                        <h3 id="add-h3">Create new notebook</h3>
                     </div>
                     <div className="add-nb-explainer">
                         <p>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</p>
                     </div>
-                    <div className="add-nb-label">
-                        <label>Name</label>
+                    <div className="label-input">
+                        <div className="add-nb-label">
+                            <label>Name</label>
+                        </div>
+                        <div className="add-nb-input">
+                            <input
+                                type="text"
+                                name="title"
+                                id="input-add"
+                                placeholder="New Notebook"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                required
+                            />
+                        </div>
                     </div>
-                    <div className="add-nb-input">
-                        <input
-                            type="text"
-                            name="title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                        />
-                    </div>
-                    <div className="add-nb-button">
-                        <button type="submit">Create</button>
+                    <div className="nb-btnsbox">
+                        <div className="cancel-nb">
+                            <button
+                                className="nb-btn one"
+                                onClick={() => setShowNew(false)}
+                                >
+                                Cancel
+                            </button>
+                        </div>
+                        <div className="add-nb-button">
+                            <button
+                                className="nb-btn two"
+                                type="submit"
+                                >
+                                Create
+                            </button>
+                        </div>
                     </div>
                 </div>
             </form>
