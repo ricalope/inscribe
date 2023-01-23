76b440526ac2cd54d73ddff34bf50c1f173cdb47
ricalope <mrrobot421@gmail.com>
2023-01-23 13:29:09 -0700

    added additional styling for edit task modal

--- react-app/src/components/DeleteTask/DeleteTaskModal.js
+++ react-app/src/components/DeleteTask/DeleteTaskModal.js
@@ -2,12 +2,12 @@ import { Modal } from '../../context/Modal';
 import DeleteTask from './index';
 
 
-function DeleteTaskModal({ taskId, showDelete, setShowDelete }) {
+function DeleteTaskModal({ taskId, showDelete, setShowDelete, setShowEdit }) {
     return (
         <>
             {showDelete && (
                 <Modal onClose={() => setShowDelete(false)}>
-                    <DeleteTask taskId={taskId} setShowDelete={setShowDelete} />
+                    <DeleteTask taskId={taskId} setShowDelete={setShowDelete} setShowEdit={setShowEdit} />
                 </Modal>
             )}
         </>
--- react-app/src/components/EditTask/EditOneTask.js
+++ react-app/src/components/EditTask/EditOneTask.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { editTaskThunk } from '../../store/task';
+import { DarkModeContext } from '../../context/ThemeContext';
 import DeleteTaskModal from '../DeleteTask/DeleteTaskModal';
 
 
 function EditOneTask({ taskId, taskBody, taskDate, setShowEdit }) {
 
     const dispatch = useDispatch();
+    const { darkMode } = useContext(DarkModeContext);
 
     const formattedDate = date => {
         date = Date.parse(date)
@@ -21,10 +23,26 @@ function EditOneTask({ taskId, taskBody, taskDate, setShowEdit }) {
 
     const [ body, setBody ] = useState(taskBody);
     const [ date, setDate ] = useState(formattedDate(taskDate));
+    const [ errors, setErrors ] = useState([]);
+    const [ submitted, setSubmitted ] = useState(false);
     const [ showDelete, setShowDelete ] = useState(false);
+
+    useEffect(() => {
+        if (body.trim().length < 1) {
+            setErrors('Task body cannot be empty. Please enter a body for the task.')
+        }
+
+        return () => {
+            setErrors([])
+            setSubmitted(false)
+        }
+    }, [ body ])
 
     const onSubmit = async e => {
         e.preventDefault()
+
+        setSubmitted(true)
+        if (errors.length > 0) return
 
         const formData = {
             taskId,
@@ -59,30 +77,39 @@ function EditOneTask({ taskId, taskBody, taskDate, setShowEdit }) {
                             onChange={(e) => setDate(e.target.value)}
                         />
                     </div>
+                    {submitted && errors.length > 0 && (
+                        <div className="errors">
+                            {errors}
+                        </div>
+                    )}
                     <div className="t-edit-btns">
                         <button
                             type="button"
                             className="t-del-button"
                             onClick={() => setShowDelete(true)}
                         >
-                            Delete
+                            Delete task
                         </button>
-                        <button
-                            type="button"
-                            onClick={() => setShowEdit(false)}>
-                            Cancel
-                        </button>
-                        <button type="submit" className="t-submit">
-                            Submit
-                        </button>
+                        <div className="t-can-sub">
+                            <button
+                                type="submit"
+                                className={darkMode ? "nb-btn one dark" : "nb-btn one light"}
+                                onClick={() => setShowEdit(false)}>
+                                Cancel
+                            </button>
+                            <button type="submit" className="nb-btn two">
+                                Submit
+                            </button>
+                            {showDelete && (
+                                <DeleteTaskModal
+                                    taskId={taskId}
+                                    showDelete={showDelete}
+                                    setShowDelete={setShowDelete}
+                                    setShowEdit={setShowEdit}
+                                />
+                            )}
+                        </div>
                     </div>
-                    {showDelete && (
-                        <DeleteTaskModal
-                            taskId={taskId}
-                            showDelete={showDelete}
-                            setShowDelete={setShowDelete}
-                        />
-                    )}
                 </div>
             </form>
         </>
