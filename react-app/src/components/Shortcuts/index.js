7b7641f34d801c0f84b462e32e429d9c7beaca7f
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-02-27 07:29:37 -0700

    added and styled shortcuts

--- react-app/src/components/Shortcuts/index.js
+++ react-app/src/components/Shortcuts/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getShortcutsThunk } from '../../store/shortcut';
 import './Shortcuts.css';
 
@@ -21,56 +22,60 @@ function Shortcuts({ showShortcuts, setShowShortcuts }) {
     useEffect(() => {
         if (showShortcuts) {
             setStyle('show')
-        } else {
+        } else if (!showShortcuts) {
             setStyle('hide')
         }
     }, [ showShortcuts ])
 
     return (
         <>
-            <div className="shortcuts-main-container">
-                <div id="shortcuts-main" className={style}>
-                    <div className="shortcuts-header">
-                        <div className="sc-title">
-                            <h2>Shortcuts</h2>
-                        </div>
-                        <div className="sc-title-close">
-                            <button className="sc-close" onClick={() => setShowShortcuts(false)}>
-                                <i className="fa-solid fa-arrow-left-long" />
-                            </button>
-                        </div>
+            <div id="shortcuts-main" className={style}>
+                <div className="shortcuts-header">
+                    <div className="sc-title">
+                        <h2>Shortcuts</h2>
                     </div>
-                    <div className="shortcuts-inner-container">
-                        <div className="sc-wrapper">
-                            <div className="sc-header">
-                                <h4 className="sc-h4">Notes</h4>
-                            </div>
-                            {notes.map(note => (
-                                <div key={note.id} className="sc-content">
+                    <div className="sc-title-close">
+                        <button className="sc-close" onClick={() => setShowShortcuts(false)}>
+                            <i className="fa-solid fa-arrow-left-long" />
+                        </button>
+                    </div>
+                </div>
+                <div className="shortcuts-inner-container">
+                    <div className="sc-wrapper">
+                        <div className="sc-header">
+                            <h4 className="sc-h4">Notes</h4>
+                        </div>
+                        {notes.map(note => (
+                            <div key={note.id} className="sc-content">
+                                <Link exact="true" to="/notes">
                                     <p className="sc-p">{note.title}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <div className="sc-wrapper">
-                            <div className="sc-header">
-                                <h4 className="sc-h4">Tasks</h4>
+                                </Link>
                             </div>
-                            {tasks.map(task => (
-                                <div key={task.id} className="sc-content">
+                        ))}
+                    </div>
+                    <div className="sc-wrapper">
+                        <div className="sc-header">
+                            <h4 className="sc-h4">Tasks</h4>
+                        </div>
+                        {tasks.map(task => (
+                            <div key={task.id} className="sc-content">
+                                <Link exact="true" to="/tasks">
                                     <p className="sc-p">{task.body}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <div className="sc-wrapper">
-                            <div className="sc-header">
-                                <h4 className="sc-h4">Notebooks</h4>
+                                </Link>
                             </div>
-                            {notebooks.map(nb => (
-                                <div className="sc-content">
-                                    <p className="sc-p">{nb.title}</p>
-                                </div>
-                            ))}
+                        ))}
+                    </div>
+                    <div className="sc-wrapper">
+                        <div className="sc-header">
+                            <h4 className="sc-h4">Notebooks</h4>
                         </div>
+                        {notebooks.map(nb => (
+                            <div className="sc-content">
+                                <Link exact="true" to={`/notebooks/${nb.id}`}>
+                                    <p className="sc-p">{nb.title}</p>
+                                </Link>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
